966917f64d80723636ab79fe7cb77bf6cb6855dd
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-07 19:35:51 +0300

    Вычистил комменты

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,6 @@ import { getUsers, createUser, getUserById, updateUser, updateUserAvatar } from 
 const router = express.Router();
 router.get('/', getUsers);
 router.get('/:id', getUserById);
-// сработает при POST-запросе на URL /films
 router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateUserAvatar);
