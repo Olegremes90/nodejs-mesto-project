4fbdcbe6adad183f1f31f5aaad4c8fb3dbd8a888
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-07 19:32:34 +0300

    Вычистил комменты + middleware-404

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,31 @@
-// app.ts — входной файл
+
 import express, {Request, Response, NextFunction} from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
 const app = express();
-/*
-по какой то причине не видит расширение интерфейса Request из файла @types/express.d.ts
-крутил по разному, но так и не получилось, в итоге оставил его здесь(
- */
+
 interface CustomError extends Error {
-  statusCode?: number; // Статус ошибки
-  message: string; // Сообщение об ошибке
+  statusCode?: number;
+  message: string;
 }
 declare global {
   namespace Express {
     interface Request {
       user: {
-        _id: string; // или другой тип, который вы используете
-        // добавьте другие поля, если необходимо
+        _id: string;
       };
     }
   }
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// подключаемся к серверу MongoDB
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   req.user = {
-    _id: '67f27562a02659d36a43659e' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '67f27562a02659d36a43659e'
   };
 
   next();
@@ -37,18 +33,21 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ message: 'Not found' });
+});
 app.use((err:CustomError, req: Request, res: Response) => {
   const { statusCode = 500, message } = err;
 
   res
     .status(statusCode)
     .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message
     });
 });
-// подключаем мидлвары, роуты и всё остальное...
+
+
 
 app.listen(3000);
