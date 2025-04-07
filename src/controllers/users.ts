89a403dfda0c245cb6d8ef8e6c92677c65736766
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-07 19:35:33 +0300

    Импорты, комменты, статусы

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/user';
-
-const NotFoundError = require('../errors/not-found-err');
-
-const IncorrectData = require('../errors/incorrect-data');
+import NotFoundError from '../errors/not-found-err';
+import IncorrectData from '../errors/incorrect-data';
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   try {
@@ -25,9 +23,9 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
     if (!avatar) {
       throw new IncorrectData('Некорректные данные в поле avatar');
     }
-    /* напишите код здесь */
+
     return User.create({ name, about, avatar })
-      .then((user) => res.send({ data: user }))
+      .then((user) => res.status(201).send({ data: user }))
       .catch(next);
   } catch (error) {
     next(error);
@@ -41,7 +39,7 @@ export const getUserById = async (req: Request, res: Response, next: NextFunctio
     if (!user) {
       throw new NotFoundError('Пользователь не найден');
     }
-    res.status(200).json(user); // Верните данные пользователя
+    res.status(200).json(user);
   } catch (err) {
    next(err)
   }
@@ -53,14 +51,11 @@ export const updateUser = async (req: Request, res: Response, next:NextFunction)
       userId = req.user._id;
     }
     const updates = req.body;
-    if(!updates.name || updates.name.length < 2 ||  updates.name.length > 30) {
+    if(updates.name.length < 2 ||  updates.name.length > 30) {
       throw new IncorrectData('Некорректные данные в поле name');
     }
-    if (!updates.about || updates.about.length < 2 ||  updates.about.length > 200) {
+    if (updates.about.length < 2 ||  updates.about.length > 200) {
       throw new IncorrectData('Некорректные данные в поле about');
-    }
-    if(!updates.avatar) {
-      throw new IncorrectData('Некорректные данные в поле avatar');
     }
     const updatedUser  = await User.findByIdAndUpdate(userId, updates, { new: true });
     if (!updatedUser ) {
