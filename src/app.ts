2fdf2c92950f170019a2a3e85625fd7f1c1751d6
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-10 23:14:03 +0300

    Подключил валидаторы к роутам, поправил мидлвар

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
 import {login, createUser} from './controllers/users';
 import  auth  from './middlewares/auth';
 import { errorLogger, requestLogger }  from './middlewares/logger';
+import {validateSignUp, validateLogin} from './validation/validation';
+import NotFoundError from './errors/not-found-err';
+import {celebrate} from "celebrate";
 const app = express();
 
 interface CustomError extends Error {
@@ -31,20 +34,20 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(requestLogger)
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate(validateLogin ), login);
+app.post('/signup', celebrate(validateSignUp ), createUser);
 
 app.use(auth);
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
-app.use((req: Request, res: Response) => {
-  res.status(404).json({ message: 'Not found' });
+app.use((req: Request, res: Response, next) => {
+  next(new NotFoundError('Маршрут не найден'));
 });
 
 app.use(errorLogger);
-app.use((err:CustomError, req: Request, res: Response) => {
+app.use((err:CustomError, req: Request, res: Response, next:NextFunction) => {
   const { statusCode = 500, message } = err;
 
   res
