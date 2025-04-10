ce45f7c5f12f6a6e6e00a1079e12f8fe66f4c91e
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-10 23:14:53 +0300

    Переделал обработку ошибки + .env

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,33 +1,51 @@
 // middlewares/auth.ts
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import {NextFunction, Request, Response} from 'express';
+import AuthorizedError from '../errors/authorized-err';
+import { config } from 'dotenv';
 
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        _id: string;
+      };
+    }
+  }
+}
 interface CustomJwtPayload extends JwtPayload {
   _id: string;
 }
 
+config();
+
+export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+
 const auth  = (req: Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
-
+  try {
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new AuthorizedError('Необходима авторизация');
   }
 
-  const token = authorization.replace('Bearer ', '');
+
   let payload;
 
-  try {
-    payload = jwt.verify(token, 'some-secret-key') as CustomJwtPayload;
+    try {
+      payload = jwt.verify(JWT_SECRET, 'some-secret-key') as CustomJwtPayload;
+      if (!payload) {
+         throw new AuthorizedError('Некорректный токен');
+         }
+       req.user = payload;
+       }catch (error){
+         if (error instanceof jwt.JsonWebTokenError) {
+           return next(new AuthorizedError('Некорректный токен'));
+         }
+            }
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Некорректный токен' });
+   next(err);
   }
-
-  req.user = payload;
-
   next();
 };
 
