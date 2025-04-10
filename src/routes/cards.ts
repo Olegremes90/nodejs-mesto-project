bd1b35ee1dffbf030136d15d6ac25fc115985736
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-10 23:16:29 +0300

    Подключение валидации

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import  { getCard, createCard, deleteCard, likeCard, dislikeCard} from '../controllers/cards';
+import {celebrate} from "celebrate";
+import {validateCreateCard} from "../validation/validation";
+
 const router = express.Router();
 router.get('/', getCard);
-router.post('/', createCard);
+router.post('/',  celebrate(validateCreateCard ), createCard);
 router.delete('/:id', deleteCard);
 router.put('/:id/likes', likeCard)
 router.delete('/:id/likes', dislikeCard)
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import  { getUsers, getUserById, updateUser, updateUserAvatar, getInfoCurrentUser } from '../controllers/users';
+import {celebrate} from 'celebrate';
+import {validateUpdateUser, validateUpdateUserAvatar} from "../validation/validation";
+
 const router = express.Router();
 router.get('/', getUsers);
+router.get('/me', getInfoCurrentUser)
+router.patch('/me',  celebrate(validateUpdateUser ),updateUser);
+router.patch('/me/avatar',celebrate(validateUpdateUserAvatar ), updateUserAvatar);
 router.get('/:id', getUserById)
-router.get('/user/me', getInfoCurrentUser)
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateUserAvatar);
-
 export default router;
