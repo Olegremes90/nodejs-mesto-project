9f28d8600110d797b378800bca381f7be3c5564a
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-12 15:45:55 +0300

    свалидировал парамметры

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import  { getUsers, getUserById, updateUser, updateUserAvatar, getInfoCurrentUser } from '../controllers/users';
 import {celebrate} from 'celebrate';
-import {validateUpdateUser, validateUpdateUserAvatar} from "../validation/validation";
+import {validateUpdateUser, validateUpdateUserAvatar, validateReqId} from "../validation/validation";
 
 const router = express.Router();
 router.get('/', getUsers);
 router.get('/me', getInfoCurrentUser)
 router.patch('/me',  celebrate(validateUpdateUser ),updateUser);
 router.patch('/me/avatar',celebrate(validateUpdateUserAvatar ), updateUserAvatar);
-router.get('/:id', getUserById)
+router.get('/:id', celebrate(validateReqId ), getUserById)
 export default router;
