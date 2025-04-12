a90c5965d6a2f1bd8a7f9c1abe18c82f5d8b8a19
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-04-12 15:43:40 +0300

    добавил celebrate на параматеры запроса

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import  { getCard, createCard, deleteCard, likeCard, dislikeCard} from '../controllers/cards';
 import {celebrate} from "celebrate";
-import {validateCreateCard} from "../validation/validation";
+import {validateCreateCard, validateReqId} from "../validation/validation";
 
 const router = express.Router();
 router.get('/', getCard);
 router.post('/',  celebrate(validateCreateCard ), createCard);
-router.delete('/:id', deleteCard);
-router.put('/:id/likes', likeCard)
-router.delete('/:id/likes', dislikeCard)
+router.delete('/:id', celebrate(validateReqId ), deleteCard);
+router.put('/:id/likes', celebrate(validateReqId ), likeCard)
+router.delete('/:id/likes', celebrate(validateReqId ), dislikeCard)
 export default router;
