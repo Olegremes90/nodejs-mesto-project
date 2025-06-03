f3ac4fb0bc2c739c008a1988a7e6a20ce3dab19a
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-06-03 21:47:08 +0300

    подправил конфиг

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'mesto-app',
-      script: 'src/app.ts',
-      interpreter: 'ts-node',
-      watch: true,
-      autorestart: true,
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
+      name: "mesto-app",
+      script: "./src/app.ts",
+      interpreter: "ts-node",
+      args: ["--loader", "ts-node/esm"],
     },
   ],
 };
