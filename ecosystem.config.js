aae0b9cbaaefb7818ee4cee085e883dbd1166c9b
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-06-13 17:36:39 +0300

    подправил конфиг

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+} = process.env;
+
 module.exports = {
-  apps: [
-    {
-      name: 'mesto-app',
-      script: './dist/app.js',   // Запускаем JS, а не TS
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production'
-      }
-    }
-  ]
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:Olegremes90/nodejs-mesto-project.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
 };
